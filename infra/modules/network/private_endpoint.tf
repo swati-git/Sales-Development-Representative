0b826c03ddb7357e1ac5ab60837cc03dd537dd8c
space <swati.phartiyal@gmail.com>
2026-06-16 16:51:02 +0530

    We need to create a private DNS zone for the container app to reach the PE of the Foundry Account and address the 403 error when the agent is trying to access the LLM

--- infra/modules/network/private_endpoint.tf
+++ infra/modules/network/private_endpoint.tf
@@ -11,6 +11,16 @@ resource "azurerm_private_endpoint" "sdr_pe" {
     subresource_names              = ["account"]
     is_manual_connection           = false
   }
+
+  # ONE zone group, BOTH zone ids. Azure creates an A record in each zone,
+  # all pointing at private IPs on this same PE NIC 
+  private_dns_zone_group {
+    name = "default"
+    private_dns_zone_ids = [
+      azurerm_private_dns_zone.sdr_dns.id,
+      azurerm_private_dns_zone.private_pe_dns_servicesai.id,
+    ]
+  }
 }
 
 
@@ -36,4 +46,17 @@ resource "azurerm_private_dns_a_record" "sdr_dns_record" {
   records             = [
     azurerm_private_endpoint.sdr_pe.private_service_connection[0].private_ip_address
   ]
+}
+
+resource "azurerm_private_dns_zone" "private_pe_dns_servicesai" {
+  name                = "privatelink.services.ai.azure.com"
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "link_servicesai" {
+  name                  = "link-services-ai"
+  resource_group_name   = var.resource_group_name
+  private_dns_zone_name = azurerm_private_dns_zone.private_pe_dns_servicesai.name
+  virtual_network_id    = azurerm_virtual_network.sdr_vnet.id
+  registration_enabled  = false
 }
